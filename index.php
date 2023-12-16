542fda8453378e84bfebf93c7b4943b5ba087359
nadiaabbad <152736044+nadiaabbad@users.noreply.github.com>
2023-12-16 21:37:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,34 +9,39 @@
     <h1>Mon Album</h1>
 
     <?php
-    require_once 'config.php';  // Include the configuration file
+    require_once 'config.php';  // Inclure le fichier de configuration
 
+    // Répertoire où les images seront stockées
     $imageDirectory = 'uploads/';
 
-    // Check if the folder exists, otherwise create it
+    // Vérifie si le dossier existe, sinon le crée
     if (!file_exists($imageDirectory)) {
         mkdir($imageDirectory, 0777, true);
     }
 
-    // Image upload processing
+    // Traitement du téléchargement de l'image
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
         $targetFile = $imageDirectory . basename($_FILES['image']['name']);
         
-        // Check if the file is an image
+        // Vérifie si le fichier est une image
         $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
         $allowedExtensions = array('jpg', 'jpeg', 'png', 'gif');
 
         if (in_array($imageFileType, $allowedExtensions)) {
             if (move_uploaded_file($_FILES['image']['tmp_name'], $targetFile)) {
-                // Save the image path and tags in the database
+                // Enregistrez le chemin de l'image et les tags dans la base de données
                 $fileName = $_FILES['image']['name'];
+                $tags = 'chat';  // Tag par défaut
 
-                // ... Your existing code to save file name and default tag in the database ...
+                // Utilisation de paramètres sécurisés
+                $insertQuery = "INSERT INTO MaTable (FileName, Tag) VALUES (?, ?)";
+                $params = array($fileName, $tags);
+                $stmt = sqlsrv_prepare($conn, $insertQuery, $params);
 
-                // Azure Custom Vision integration
-                include 'custom_vision_integration.php';  // Include the code for Azure Custom Vision
-                perform_custom_vision_integration($targetFile);
-                
+                if (sqlsrv_execute($stmt) === false) {
+                    die(print_r(sqlsrv_errors(), true));
+                }
+
                 echo '<p>L\'image a été téléchargée avec succès.</p>';
             } else {
                 echo '<p>Une erreur s\'est produite lors du téléchargement de l\'image.</p>';
@@ -45,32 +50,20 @@
             echo '<p>Seuls les fichiers JPG, JPEG, PNG et GIF sont autorisés.</p>';
         }
     }
-
-    // Handle the form submission to refresh the page
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['refresh'])) {
-        // Redirect to the same page to refresh
-        header("Location: {$_SERVER['PHP_SELF']}");
-        exit;
-    }
     ?>
 
-    <!-- Image upload form -->
+    <!-- Formulaire pour télécharger une image -->
     <form action="" method="post" enctype="multipart/form-data">
         <label for="image">Sélectionnez une image à télécharger :</label>
         <input type="file" name="image" id="image" accept="image/*" required>
         <button type="submit">Télécharger</button>
     </form>
 
-    <!-- Refresh button form -->
-    <form action="" method="post">
-        <button type="submit" name="refresh">Actualiser</button>
-    </form>
-
-    <!-- Display the album -->
+    <!-- Affichage de l'album -->
     <h2>Album</h2>
     <div>
         <?php
-        // Display all images in the directory
+        // Affiche toutes les images dans le répertoire
         $images = glob($imageDirectory . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
         foreach ($images as $image) {
             echo '<img src="' . $image . '" alt="Album Image">';
@@ -78,15 +71,17 @@
         ?>
     </div>
 
-    <!-- Test the database connection -->
+    <!-- Test de la connexion à la base de données -->
     <?php
-    $query = "SELECT TOP 10 Id, FileName, Tag FROM MaTable";
+    // Requête de test
+    $query = "SELECT TOP 1 Id, FileName, Tag FROM MaTable";
     $result = sqlsrv_query($conn, $query);
 
     if ($result === false) {
         die(print_r(sqlsrv_errors(), true));
     }
 
+    // Affichage des résultats de test
     while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
         echo '<p>ID: ' . $row['Id'] . ', FileName: ' . $row['FileName'] . ', Tag: ' . $row['Tag'] . '</p>';
     }
