e3040cf91b4603258c4e739567ba08acaaf9e964
nadiaabbad <152736044+nadiaabbad@users.noreply.github.com>
2023-12-17 16:54:04 +0100

    Update config.php

--- config.php
+++ config.php
@@ -24,45 +24,6 @@ if (!$conn) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
-$serverName = "tcp:projetcloudvf.database.windows.net,1433";
-$connectionOptions = array(
-    "Database" => "projetcloudvf",
-    "Uid" => "azureuser",
-    "PWD" => "123Klerviaadam",
-    "MultipleActiveResultSets" => false,
-    "Encrypt" => true,
-    "TrustServerCertificate" => false
-);
-
-// Establish the database connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
-// Check the connection
-if ($conn) {
-    // Display a generic error message to the user
-    die('Unable to connect to the database. Please try again later.');
-
     // For debugging purposes, you can uncomment the line below
     // die(print_r(sqlsrv_errors(), true));
 }
