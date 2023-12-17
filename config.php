8cfb9402ab33583af1d779b220e4ac52bd2696e2
nadiaabbad <152736044+nadiaabbad@users.noreply.github.com>
2023-12-17 16:49:47 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,52 @@
 <?php
 $serverName = "tcp:projetcloudvf.database.windows.net,1433";
 $connectionOptions = array(
+    "Database" => "projetcloudfinal",
+    "Uid" => "azureuser",
+    "PWD" => "123Klerviaadam",
+    "MultipleActiveResultSets" => false,
+    "Encrypt" => true,
+    "TrustServerCertificate" => false
+);
+
+// Établir la connexion à la base de données
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+// Vérifier la connexion
+if (!$conn) {
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+$serverName = "tcp:projetcloudvf.database.windows.net,1433";
+$connectionOptions = array(
     "Database" => "projetcloudvf",
     "Uid" => "azureuser",
     "PWD" => "123Klerviaadam",
