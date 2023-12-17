242ff998796f0c2c7b9f7dcae7a3f59614b1da3f
nadiaabbad <152736044+nadiaabbad@users.noreply.github.com>
2023-12-17 16:53:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <?php
     require_once 'config.php';  // Include the configuration file
 
-    // Directory where images will be stored
+    // Directory where the images will be stored
     $imageDirectory = 'uploads/';
 
     // Check if the directory exists, create it if not
@@ -19,7 +19,7 @@
         mkdir($imageDirectory, 0777, true);
     }
 
-    // Image upload handling
+    // Image upload processing
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
         $targetFile = $imageDirectory . basename($_FILES['image']['name']);
         
@@ -34,9 +34,9 @@
 
                 // Call Custom Vision for image classification
                 $imagePath = $imageDirectory . $fileName;
-                $customVisionEndpoint = "https://projetcloudclass.cognitiveservices.azure.com/";
-                $customVisionPredictionKey = "556885fbb73242b2b577ced53b0d4952";
-                $customVisionIterationId = "9121e358-9377-47c7-a312-3f44aa69ddd7";
+                $customVisionEndpoint = "https://projetcloudclass.cognitiveservices.azure.com/"; // Replace with your Custom Vision endpoint
+                $customVisionPredictionKey = "556885fbb73242b2b577ced53b0d4952"; // Replace with your Custom Vision prediction key
+                $customVisionIterationId = "9121e358-9377-47c7-a312-3f44aa69ddd7"; // Replace with your Custom Vision iteration ID
 
                 // Create a POST request to the Custom Vision prediction endpoint
                 $ch = curl_init($customVisionEndpoint . "/customvision/v3.0/Prediction/$customVisionIterationId/classify/iterations/Iteration1/image");
@@ -73,21 +73,21 @@
                     die(print_r(sqlsrv_errors(), true));
                 }
 
-                echo '<p>L\'image a été téléchargée et classifiée avec succès.</p>';
+                echo '<p>The image was uploaded and classified successfully.</p>';
             } else {
-                echo '<p>Une erreur s\'est produite lors du téléchargement de l\'image.</p>';
+                echo '<p>An error occurred while uploading the image.</p>';
             }
         } else {
-            echo '<p>Seuls les fichiers JPG, JPEG, PNG et GIF sont autorisés.</p>';
+            echo '<p>Only JPG, JPEG, PNG, and GIF files are allowed.</p>';
         }
     }
     ?>
 
-    <!-- Form to upload an image -->
+    <!-- Form for uploading an image -->
     <form action="" method="post" enctype="multipart/form-data">
-        <label for="image">Sélectionnez une image à télécharger :</label>
+        <label for="image">Select an image to upload:</label>
         <input type="file" name="image" id="image" accept="image/*" required>
-        <button type="submit">Télécharger</button>
+        <button type="submit">Upload</button>
     </form>
 
     <!-- Display the album -->
@@ -105,7 +105,7 @@
     <!-- Test the database connection -->
     <?php
     // Test query
-    $query = "SELECT TOP 20 ImageId, FileName, Tag FROM MaTable";
+    $query = "SELECT TOP 30 ImageID, FileName, Tag FROM MaTable";
     $result = sqlsrv_query($conn, $query);
 
     if ($result === false) {
@@ -114,8 +114,10 @@
 
     // Display test results
     while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-        echo '<p>ImageID: ' . $row['ImageId'] . ', FileName: ' . $row['FileName'] . ', Tag: ' . $row['Tag'] . '</p>';
+        echo '<p>ImageID: ' . $row['ImageID'] . ', FileName: ' . $row['FileName'] . ', Tag: ' . $row['Tag'] . '</p>';
     }
     ?>
 </body>
 </html>
+
+ 
